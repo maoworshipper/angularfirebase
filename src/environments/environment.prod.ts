95a194fc792648d6ee879c57922f3b24fe5fb473
maoworshipper <maoworshipper@gmail.com>
2021-05-23 00:04:03 -0500

    ToDo Angular Firebase Deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  apiKey: "AIzaSyDjvCxmSEv-EGMkUJEQElzkOqRyb7ByRF8",
+  authDomain: "kanbanfire-848aa.firebaseapp.com",
+  projectId: "kanbanfire-848aa",
+  storageBucket: "kanbanfire-848aa.appspot.com",
+  messagingSenderId: "851357404616",
+  appId: "1:851357404616:web:776a58ddfc02f26c1b44fd",
+  measurementId: "G-5D80S5TR14"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyDjvCxmSEv-EGMkUJEQElzkOqRyb7ByRF8",
+  authDomain: "kanbanfire-848aa.firebaseapp.com",
+  projectId: "kanbanfire-848aa",
+  storageBucket: "kanbanfire-848aa.appspot.com",
+  messagingSenderId: "851357404616",
+  appId: "1:851357404616:web:776a58ddfc02f26c1b44fd",
+  measurementId: "G-5D80S5TR14"
+  }
 };
 
 /*
@@ -14,3 +23,4 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
